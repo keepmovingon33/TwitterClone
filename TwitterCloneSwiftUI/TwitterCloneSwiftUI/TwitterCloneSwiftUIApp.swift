b957d454009acfb3ef04e5245379157d32c2708e
sky <anvanc37@gmail.com>
2022-02-20 20:46:24 -0500

    setup project

---
+++ TwitterCloneSwiftUI/TwitterCloneSwiftUI/TwitterCloneSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneSwiftUIApp.swift
+//  TwitterCloneSwiftUI
+//
+//  Created by sky on 2/20/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
